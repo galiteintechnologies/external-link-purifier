90f79a65f0c0a39d6293f64f04c8ca0c2808c5c5
nexuslinkservices <bhavin.nakrani@nexuslinkservices.in>
2016-08-27 17:27:27 +0530

    Try to resolve scrutinizer issue

--- src/LinkPurifier.php
+++ src/LinkPurifier.php
@@ -23,7 +23,7 @@ class LinkPurifier {
         if (!empty($pathToYml)) {
             $this->pathToYml = $pathToYml;
         } else {
-            $this->pathToYml = __DIR__ . "\Resources\config\config.yml";
+            $this->pathToYml = __DIR__ . "/Resources/config/config.yml";
         }
     }
 
